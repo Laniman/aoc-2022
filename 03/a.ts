503a01c87a3fe0a72a51796f8bacca24f526601f
George Achkasov <georgij-net@yandex.ru>
2022-12-03 19:02:00 +0400

    add day 3 solution

---
+++ 03/a.ts
@@ -0,0 +1,51 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+
+const scoreLetter = (letter: string) => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return alphabet.indexOf(letter) + 1;
+};
+
+function solution(input: string) {
+  const lines = input.split("\n");
+  let sum = 0;
+
+  lines.forEach((rucksack) => {
+    const half = rucksack.length / 2;
+    const slice = rucksack.slice(half, rucksack.length);
+    const set = new Set(slice);
+    const char = rucksack
+      .slice(0, half)
+      .split("")
+      .find((char) => {
+        return set.has(char);
+      })!;
+
+    sum += scoreLetter(char);
+  });
+
+  return sum;
+}
+
+Deno.test("example", () => {
+  const input =
+    "vJrwpWtwJgWrhcsFMMfFFhFp\n" +
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n" +
+    "PmmdzqPrVvPwwTWBwg\n" +
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n" +
+    "ttgJtRGJQctTZtZT\n" +
+    "CrZsJsPPZsGzwwsLwLmpwMDw";
+
+  const actual = solution(input);
+  const expected = 157;
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("puzzle input", { ignore: false }, () => {
+  const input = Deno.readTextFileSync("./03/input.txt");
+
+  const actual = solution(input);
+  const expected = 7831;
+
+  assertEquals(actual, expected);
+});
---
+++ 03/b.ts
@@ -0,0 +1,64 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+
+const scoreLetter = (letter: string) => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return alphabet.indexOf(letter) + 1;
+};
+
+function solution(input: string) {
+  const lines = input.split("\n");
+
+  const [chunked] = lines.reduce<[string[][], string[]]>(
+    ([acc, chunk], line) => {
+      chunk.push(line);
+
+      if (chunk.length === 3) {
+        acc.push(chunk);
+        return [acc, []];
+      }
+
+      return [acc, chunk];
+    },
+    [[], []]
+  );
+
+  const result = chunked
+    .map((chunk) => {
+      const [first, second, third] = chunk
+        .map((line) => line.split(""))
+        .map((line) => new Set(line));
+
+      for (const s of first) {
+        if (second.has(s) && third.has(s)) {
+          return s;
+        }
+      }
+    })
+    .reduce((acc, s) => acc + scoreLetter(s!), 0);
+
+  return result;
+}
+
+Deno.test("example", () => {
+  const input =
+    "vJrwpWtwJgWrhcsFMMfFFhFp\n" +
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n" +
+    "PmmdzqPrVvPwwTWBwg\n" +
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n" +
+    "ttgJtRGJQctTZtZT\n" +
+    "CrZsJsPPZsGzwwsLwLmpwMDw";
+
+  const actual = solution(input);
+  const expected = 70;
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("puzzle input", { ignore: false }, () => {
+  const input = Deno.readTextFileSync("./03/input.txt");
+
+  const actual = solution(input);
+  const expected = 2683;
+
+  assertEquals(actual, expected);
+});
